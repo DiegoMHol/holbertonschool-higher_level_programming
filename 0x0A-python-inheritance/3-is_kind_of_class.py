b7f178e2a00fdee9ab0d4d528cfe72cb6434ebe6
Diego Monfort <diegomonfort12346@gmail.com>
2022-01-31 14:13:38 -0800

    Is instance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Python poryect """
+
+
+def is_kind_of_class(obj, a_class):
+    """ Return true if class and obj """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
