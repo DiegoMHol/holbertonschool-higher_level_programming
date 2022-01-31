87699bf4138dc3b96f92e87277248b178cd6d358
Diego Monfort <diegomonfort12346@gmail.com>
2022-01-31 14:04:53 -0800

    is same calss

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" ptyhon proyect """
+
+
 def is_same_class(obj, a_class):
+    """ Return true if class is obj """
     if type(obj) is a_class:
         return True
     else:
