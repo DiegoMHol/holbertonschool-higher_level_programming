e97f63c67f6c2a2137074fd0f8258dce5d645d73
Diego Monfort <diegomonfort12346@gmail.com>
2022-01-31 17:56:16 -0800

    Area, str

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Pythin proyect """
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Empty class """
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+    
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return ("[Rectangle] {:d}/{:d}".format(self.__width, self.__height))
