3650e75c46bdba689af1a3cef964cfc7be198d06
Diego Monfort <diegomonfort12346@gmail.com>
2022-01-31 18:18:47 -0800

    Inheritance

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Pythin proyect """
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """ Square inheritance """
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return ("[Square] {:d}/{:d}".format(self.__size, self.__size))
