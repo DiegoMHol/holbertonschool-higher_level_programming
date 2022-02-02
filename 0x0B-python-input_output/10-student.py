49231d35de2b9930f6224499fa5e71db967ff292
Diego Monfort <diegomonfort12346@gmail.com>
2022-02-01 17:51:33 -0800

    Atttrs

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Python Proyect """
+
+
+class Student:
+    """ Create a strudent class """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if not isinstance(attrs, list):
+            return self.__dict__
+        for i in attrs:
+            if not isinstance(i, str):
+                return self.__dict__
+        dic = {}
+        for j in attrs:
+            if j in self.__dict__.keys():
+                dic[j] = self.__dict__[j]
+        return dic
