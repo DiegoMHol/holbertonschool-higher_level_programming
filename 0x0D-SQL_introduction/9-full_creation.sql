7ca03dc7c4d9bf915eef29229b228cc1b33549c9
Diego Monfort <diegomonfort12346@gmail.com>
2022-03-08 08:47:11 -0800

    [CREATE TABLE]

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- Write a script that creates a table second_table in the database
+CREATE TABLE IF NOT EXISTS second_table(id int, name varchar(256), score int);
+INSERT INTO second_table VALUES (1, "Jhon", 10);
+INSERT INTO second_table VALUES (2, "Alex", 3);
+INSERT INTO second_table VALUES (3, "Bob", 14);
+INSERT INTO second_table VALUES (4, "George", 8);
