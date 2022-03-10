5c4a45278db41b8bce611c7e3cc25bf658ca8cad
Diego Monfort <diegomonfort12346@gmail.com>
2022-03-09 17:24:17 -0800

    [DUMBP]

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
-i-- Import the database dump from hbtn_0d_tvshows
+-- Import the database dump from hbtn_0d_tvshows
 SELECT s.title, g.genre_id AS genre_id FROM tv_shows AS s RIGHT JOIN tv_show_genres AS g ON g.show_id = s.id ORDER BY s.title, g.genre_id;
