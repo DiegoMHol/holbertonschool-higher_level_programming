4400ff72abddfaa711e2a2e416b1e0489826b8c2
Diego Monfort <diegomonfort12346@gmail.com>
2022-04-04 16:32:59 -0700

    [FIX STANDARD]

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,6 +3,6 @@ const args = process.argv;
 const first = parseInt(args[2]);
 const second = parseInt(args[3]);
 function add (first, second) {
-	console.log(first + second);
+  console.log(first + second);
 }
 add(first, second);
