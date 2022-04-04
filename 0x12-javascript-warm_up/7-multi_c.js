80b6b88f07bb8d5a3aa40ce4331b3bdbc70627b2
Diego Monfort <diegomonfort12346@gmail.com>
2022-04-04 16:22:55 -0700

    [PRINT X TIMES]

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const args = process.argv;
+const counter = parseInt(args[2]);
+if (isNaN(counter)) {
+	console.log('Missing number of occurrences')
+}
+for (let index = 0; index < counter; index++){
+	console.log('C is fun')
+}
