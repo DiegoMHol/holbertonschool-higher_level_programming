1ccdbbfe8e77ca05bed1d096bc864ee6f24585a7
Diego Monfort <diegomonfort12346@gmail.com>
2022-04-04 16:19:19 -0700

    [FIX CODE]

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const string = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let index = 0; index < string.lenght; index++) {
+for (let index = 0; index < string.length; index++) {
   const element = string[index];
   console.log(element);
 }
