99c5e91399131c5e220e2ee85adc072ae6a4b3e3
Diego Monfort <diegomonfort12346@gmail.com>
2022-04-04 19:32:23 -0700

    [FIX FACTORIAL]

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 const args = process.argv;
 if (args.length < 2 || isNaN(parseInt(args[2]))) {
-	console.log(factorial(1));
-}else{
-	console.log(factorial(parseInt(args[2])));
+  console.log(factorial(1));
+} else {
+  console.log(factorial(parseInt(args[2])));
 }
-function factorial(n) {
-	if (n < 0)
-		return;
-	if (n === 1)
-		return 1;
-	return n * factorial(n - 1);
+function factorial (n) {
+  if (n < 0) { return; }
+  if (n === 1) { return 1; }
+  return n * factorial(n - 1);
 }
