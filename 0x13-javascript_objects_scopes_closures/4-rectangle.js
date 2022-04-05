ceda9ae876d0435ef52c57128fcf5fb7a6ac677b
Diego Monfort <diegomonfort12346@gmail.com>
2022-04-05 15:13:40 -0700

    [FIX CODE]

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -9,7 +9,7 @@ module.exports = class Rectangle {
   }
 
   double () {
-    [this.width, this.height] = [this.width + 2, this.height * 2];
+    [this.width, this.height] = [this.width * 2, this.height * 2];
   }
 
   rotate () {
