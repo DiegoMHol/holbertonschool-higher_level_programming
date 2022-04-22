2ddb34b9564cd5094ddba5aa0ef91bee289bcbea
Diego Monfort <diegomonfort12346@gmail.com>
2022-04-22 13:27:58 -0700

    [FIX SYN]

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL as an argument, sends a GET request to the URL,
-curl -s "X-HolbertonSchool-User-Id:98" GET -sLX "$1"
+curl -s "X-HolbertonSchool-User-Id:98" -sLX GET "$1"
