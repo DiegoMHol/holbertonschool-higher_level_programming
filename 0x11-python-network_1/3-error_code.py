6f85febb5b7c9876970cca279909f590ea63115b
Diego Monfort <diegomonfort12346@gmail.com>
2022-04-23 14:59:17 -0700

    [RESPONSE]

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" sends a request to the URL and displays the body of the response """
+if __name__ == '__main__':
+    import urllib.request
+    import sys
+    from urllib.error import URLError
+
+    request = urllib.request.Request(sys.argv[1])
+    
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode(encoding='utf-8'))
+    except URLError as error:
+        print('Error code: {}'.format(error.getcode()))
