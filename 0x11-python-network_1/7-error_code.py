8e828b51e4d8cafb85b65e464aa69c8a5fec8cfa
Diego Monfort <diegomonfort12346@gmail.com>
2022-04-24 18:47:43 -0700

    [DISPLAY BODY RESPONSE]

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" script that takes in a URL, displays the body of the response"""
+if __name__ == '__main__':
+    import sys
+    import requests
+
+    try:
+        response = requests.get(sys.argv[1])
+        response.raise_for_status()
+        print(response.text)
+    except requests.exceptions.HTTPError as error:
+        print('Error code: {}'.format(error.response.status_code))
