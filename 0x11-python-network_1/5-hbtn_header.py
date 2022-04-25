79643748123715eca92e63c7f1c015f483e29566
Diego Monfort <diegomonfort12346@gmail.com>
2022-04-24 18:14:47 -0700

    [DISPLAY REQUEST ID]

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" script that fetches URL """
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    response = requests.get(sys.argv[1])
+    print(response.headers.get('X-Request-Id'))
